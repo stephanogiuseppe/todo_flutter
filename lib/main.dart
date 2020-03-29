6e8f6d80f2552c8f362a9fea1965411b847eee58
stephanogiuseppe <stephano.grp@gmail.com>
2020-03-29 18:17:19 -0300

    save and read file data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
@@ -10,9 +13,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _toDo = [];
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
-}
 
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/data.json}');
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDo);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+}
